ef73192a8fbd83e15faa3a0850a47076394a8be4
scheller <scheller.sanchez@gmail.com>
2012-10-21 02:03:18 -1000

    successfully created a user table with attributes username and email

--- db/migrate/20121021113708_create_users.rb
+++ db/migrate/20121021113708_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
-
-      t.timestamps
+      t.string      :username
+      t.string      :email
+      t.timestamps  :created_at
+      t.timestamps  :updated_at
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
