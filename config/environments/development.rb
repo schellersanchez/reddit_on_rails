1c3a9bc8bd02b257bd563d1560874548924622f4
scheller <scheller.sanchez@gmail.com>
2012-10-27 01:37:18 -1000

    Created Users table, installed Devise, configured Devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ RedditOnRails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Required line for devise gem
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
