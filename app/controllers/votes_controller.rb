eada2afae20cd13144d3a7b4bd3b059a041f7ab2
scheller <scheller.sanchez@gmail.com>
2012-11-17 17:59:48 -1000

    Modified Homepage to limit user to upvote/downvote just once per link.  Added logic to show user what vote was selected for the link.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,7 +7,16 @@ class VotesController < ApplicationController
   end
   def create
     authenticate_user!
-    @vote = current_user.votes.create(params[:vote])
+
+    # if vote exists, edit the vote.
+    # else create new vote
+    @vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+    if @vote
+      @vote.up = params[:vote][:up]
+      @vote.save
+    else
+     @vote = current_user.votes.create(params[:vote])
+    end
     redirect_to :back
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,8 @@
 class Vote < ActiveRecord::Base
   attr_accessible :up, :user_id, :link_id
+  validates :user_id, :uniqueness => { :scope => :link_id }
+
   belongs_to :user
   belongs_to :link
+
 end
